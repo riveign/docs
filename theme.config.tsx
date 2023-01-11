df7f93a8ae75ee4ccc5f64c45f03fd1532c410d1
Ignacio Rviera <riveign@gmail.com>
2023-01-11 14:04:12 +0100

    initial doc

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,25 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <>
+      <span>Fleek</span>
+    </>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/fleekHQ/fleek',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/fleekxyz',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/fleekHQ/fleek',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Fleek',
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Fleek'
+    }
   },
 }
 
