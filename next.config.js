94161231d03837dd6ea2dc6a5d97ae8b43c5e036
Ignacio Rviera <riveign@gmail.com>
2023-01-17 13:07:38 +0100

    fleek deploy config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  images: {
-    unoptimized: true,
-  },
 })
 
 module.exports = withNextra()
+module.exports = {
+  images: {
+    unoptimized: true,
+  },
+}
